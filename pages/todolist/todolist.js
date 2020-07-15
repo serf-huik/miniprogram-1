8d64d416600d23831c53d86c30de4b7e5239084c
serf-huik <1306201186@qq.com>
2020-07-15 22:06:54 +0800

    2020.7.15.2207

--- pages/todolist/todolist.js
+++ pages/todolist/todolist.js
@@ -1,28 +1,65 @@
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    msg:"hello world",
-    num:"10000",
-    isgirl:false,
-    person:{
-      age:74,
-      weight:60,
-      height:167,
-    },
-    isChecked:false,
-    list:[
-      {
-        id:0,
-        name:"000"
-      },
-      {
-        id:1,
-        name:"111"
-      },
-      {
-        id:2,
-        name:"222"
-      }
-    ],
-    istrue:true
- }
-});
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
